e5f57cb4ebec0059b24eb121e8f14f667a243a62
CorbinFoucart <corbin.foucart@gmail.com>
2014-10-21 17:56:04 -0700

    Database is almost done, need search

--- hw3DBSudoku/hw3DBSudoku/src/MetropolisFrame.java
+++ hw3DBSudoku/hw3DBSudoku/src/MetropolisFrame.java
@@ -1,16 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.TableModelEvent;
 
 import assign3.SudokuFrame;
 
 import java.awt.event.*;
 import java.io.*;
 
-public class MetropolisFrame extends JFrame {
+public class MetropolisFrame extends JFrame{
 
 	//ivars
 	private MetropolisTableModel model;
 	private JTable table;
+	private JButton add;
+	private JButton search;
+	private JTextField MetSearch;
+	private JTextField ConSearch;
+	private JTextField PopSearch;
+	private JLabel errorMessage;
 	
 	JComponent content; 
 	
@@ -19,51 +27,151 @@ public class MetropolisFrame extends JFrame {
 		super("Metropolis Viewer");
 		
 		content = (JComponent)getContentPane();
-		content.setLayout(new BorderLayout(6,6));
+		content.setLayout(new BorderLayout(10,10));
 		
 		// Create a table model
 		model = new MetropolisTableModel();
 		
 		// Create a table using that model
-		table = new JTable(model);
-		
+		makeTable();		
 		
 		// Non Table Components
-		
-		// Search Bar
-		JPanel searchBar = new JPanel();
-		
-		JLabel MetLabel = new JLabel("Metropolis: "); 
-		searchBar.add(MetLabel);
-		JTextField MetSearch = new JTextField();
-		MetSearch.setPreferredSize( new Dimension( 200, 24 ) );
-		searchBar.add(MetSearch);
-		
-		JLabel ConLabel = new JLabel("Continent: "); 
-		searchBar.add(ConLabel);
-		JTextField ConSearch = new JTextField();
-		ConSearch.setPreferredSize( new Dimension( 200, 24 ) );
-		searchBar.add(ConSearch);
-		
-		JLabel PopLabel = new JLabel("Population: "); 
-		searchBar.add(PopLabel);
-		JTextField PopSearch = new JTextField();
-		PopSearch.setPreferredSize( new Dimension( 200, 24 ) );
-		searchBar.add(PopSearch);
-		
-		this.add(searchBar, BorderLayout.NORTH);
-		
-		// Side Panel
-		JPanel sideBar = new JPanel();
+		makeSearchBar();
+		makeSidePanel();
 		
 		
-		
+		// Display the Frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
 
 	}
 	
+	// ----------------------------------- Initialization of GUI -------------------------------------- //
+	
+	public void makeTable() {
+		table = new JTable(model);
+		JScrollPane pane = new JScrollPane(table);
+		this.add(pane, BorderLayout.CENTER);
+		
+	}
+
+	public void makeSearchBar() {
+		JPanel searchBar = new JPanel();
+		
+		JLabel MetLabel = new JLabel("Metropolis: "); 
+		searchBar.add(MetLabel);
+		MetSearch = new JTextField();
+		MetSearch.setPreferredSize( new Dimension( 200, 24 ) );
+		searchBar.add(MetSearch);
+		
+		JLabel ConLabel = new JLabel("Continent: "); 
+		searchBar.add(ConLabel);
+		ConSearch = new JTextField();
+		ConSearch.setPreferredSize( new Dimension( 200, 24 ) );
+		searchBar.add(ConSearch);
+		
+		JLabel PopLabel = new JLabel("Population: "); 
+		searchBar.add(PopLabel);
+		PopSearch = new JTextField();
+		PopSearch.setPreferredSize( new Dimension( 200, 24 ) );
+		searchBar.add(PopSearch);
+		
+		this.add(searchBar, BorderLayout.NORTH);
+	}
+	
+	public void makeSidePanel() {
+		
+		JPanel sideBar = new JPanel(new GridLayout(3,1,20,20));
+		
+		// Buttons
+		JPanel sideBarButtons = new JPanel(new GridLayout(2,1));
+		JButton add = new JButton("Add");
+		add.setPreferredSize( new Dimension(80, 30) );
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String met = MetSearch.getText();
+				String con = ConSearch.getText();
+				String pop = PopSearch.getText();
+				
+				// in case of incomplete entry
+				if (fieldEmpty()) {
+					errorMessage.setText("Enter a value in all fields!");
+				}else {
+					model.add(met, con, pop);
+					errorMessage.setText("");
+				}
+				
+				clearSearchBar();
+				
+			}
+		});
+		
+		// search button
+		JButton search = new JButton("Search");
+		search.setPreferredSize( new Dimension(80, 30) );
+		search.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (fieldsEmpty()) {
+					model.getDatabase();
+				}
+			}
+		});
+			
+		// add buttons to sidebar
+		sideBarButtons.add(add);
+		sideBarButtons.add(search);
+		sideBar.add(sideBarButtons);
+		
+		// Dropdowns
+		String[] popStrings = { "Population larger than", "Population smaller than or equal to"};
+		String[] searchStrings = {"Exact Search", "Partial Search"};
+
+		//Create the dropdowns
+		JPanel sideBarDropDowns = new JPanel(new GridLayout(3,1));
+		JComboBox popList = new JComboBox(popStrings);
+		popList.setSelectedIndex(0);
+		JComboBox searchList = new JComboBox(searchStrings);
+		searchList.setSelectedIndex(0);
+		sideBarDropDowns.add(popList, BorderLayout.NORTH);
+		sideBarDropDowns.add(searchList, BorderLayout.SOUTH);
+		sideBarDropDowns.setBorder(new TitledBorder("Search Options"));
+		sideBar.add(sideBarDropDowns);
+		
+		// error message space
+		errorMessage = new JLabel("");
+		errorMessage.setPreferredSize( new Dimension(175, 30) );
+		sideBar.add(errorMessage);
+		
+		// add our beautiful panel to GUI
+		this.add(sideBar, BorderLayout.EAST);
+	}
+	
+	// returns true if at least one textfield is empty
+	public boolean fieldEmpty() {
+		String met = MetSearch.getText();
+		String con = ConSearch.getText();
+		String pop = PopSearch.getText();
+		return (met.equals("") || con.equals("") || pop.equals(""));
+	}
+	
+	// returns true if all textfields are empty
+	public boolean fieldsEmpty() {
+		String met = MetSearch.getText();
+		String con = ConSearch.getText();
+		String pop = PopSearch.getText();
+		return (met.equals("") && con.equals("") && pop.equals(""));
+	}
+	
+	public void clearSearchBar() {
+		// make the search bar blank again
+		MetSearch.setText("");
+		ConSearch.setText("");
+		PopSearch.setText("");
+	}
+	
+	
+	// ----------------------------------- main ------------------------------------- //
 	
 	public static void main(String[] args) {
 		// GUI Look And Feel
--- hw3DBSudoku/hw3DBSudoku/src/MetropolisTableModel.java
+++ hw3DBSudoku/hw3DBSudoku/src/MetropolisTableModel.java
@@ -1,5 +1,10 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
  
 import javax.swing.JFrame;
@@ -8,36 +13,173 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 
+import java.sql.ResultSetMetaData;
+
 import assign3.MyDBInfo;
 
 
 public class MetropolisTableModel extends AbstractTableModel {
 	
 	private ArrayList<String> colNames;
-	private ArrayList<ArrayList> data;
+//	private ArrayList<ArrayList> data;
+//	private ArrayList<String> nameData;
+//	private ArrayList<String> contData;
+//	private ArrayList<Long> popData;
+	private Connection con;
+	private ResultSet rs;
 	
 	// ctor
 	public MetropolisTableModel() {
-		colNames = new ArrayList<String>();
-		data = new ArrayList<ArrayList>();
+
+		// construct column names
+		colNames = new ArrayList<String>(
+				Arrays.asList("Metropolis", "Continent", "Population"));
+		
+		// connection to database
+		try {
+			// make sure JDBC driver is loaded
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// create connection between our account and the database
+			con = DriverManager.getConnection
+					("jdbc:mysql://" + server, account, password);
+						
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+			rs = stmt.executeQuery("SELECT * FROM metropolises WHERE 1=2;");
+					
+		} catch (SQLException e){
+			e.printStackTrace();
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
 	}
+	
+	// adds data from the text fields to the database
+	public void add(String metropolis, String continent, String population) {
+		System.out.println(metropolis);
+		System.out.println(continent);
+		System.out.println(population);
+		
+		try {
+			Statement stmt;
+			stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+			String insStr = buildInsertString(metropolis, continent, population);
+			System.out.println(insStr);
+			stmt.executeUpdate(insStr);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		this.fireTableDataChanged();
+	}
+	
+		private String buildInsertString(String met, String con, String pop) {
+			String insertString = "";
+			String values = " VALUES(\"" + met + "\", \"" + con + "\", \"" + pop + "\");";
+			insertString += "INSERT INTO " + dbase_name + values; 
+			return insertString;
+		}
+	
+	// gets entire database, saves it to model ivars
+	public void getDatabase() {
+		try {
+			Statement stmt;
+			stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+			rs = stmt.executeQuery("SELECT * FROM metropolises ORDER BY metropolis;");
+			
+			while(rs.next()) {
+				String name = rs.getString("metropolis");
+				String conName = rs.getString("continent");
+				long pop = rs.getLong("population");
+				
+				System.out.println(name + "\t" + conName + "\t" + pop);
+			}
+			this.fireTableDataChanged();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void exactSearch(String metr, String cont, Long popu, boolean greater) {
+		String query = buildExactString(metr, cont, popu, greater);
+		System.out.println(query);
+	}
+	
+		public String buildExactString(String metr, String cont, Long popu, boolean greater) {
+			boolean first = true;
+			String qString = "SELECT * FROM " + dbase_name + " ";
+			if (!metr.equals("")) {
+				first = false;
+				qString += "WHERE metropolis = \"" + metr + "\"";
+			}
+			
+			if (!cont.equals("")) {
+				if (first) {
+					first = false;
+					qString += " WHERE continent = \"" + cont + "\"";
+				}else  {
+					qString += " AND WHERE continent = \"" + cont + "\"";
+				}
+			}
+			
+			if (popu != null) {
+				if (first) {
+					first = false;
+					qString += " WHERE population = \"" + popu + "\"";
+				}else  {
+					qString += " AND WHERE continent = \"" + popu + "\"";
+				}
+			}
+			
+			qString += ";";
+			
+			return qString;
+		}
+	
+	// ----------------------------- Overridden Methods ---------------------------------//
 	
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
+		try {
+			rs.last();
+			int rows = rs.getRow();
+			rs.beforeFirst();
+			return rows;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 		return 0;
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
+		try {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int colNum = rsmd.getColumnCount();
+			return colNum;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return 0;
+	}
+	
+	@Override
+	public String getColumnName(int i) {
+		return colNames.get(i);
 	}
 
 	@Override
 	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			rs.absolute(arg0 + 1);
+			return rs.getObject(arg1 + 1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
 
@@ -46,6 +188,7 @@ public class MetropolisTableModel extends AbstractTableModel {
 	static String password = MyDBInfo.MYSQL_PASSWORD;
 	static String server = MyDBInfo.MYSQL_DATABASE_SERVER;
 	static String database = MyDBInfo.MYSQL_DATABASE_NAME;
+	static String dbase_name = "metropolises";
 	
 	
 }
